7a3fd82c48cb3fd95d72d996f96f481cb4140506
Josip Taylor <josiptaylor@gmail.com>
2026-02-23 10:08:34 -0600

    update image for Tequeños Doña Ana to a center aligned one

--- src/App.jsx
+++ src/App.jsx
@@ -475,8 +475,7 @@ const MOCK_DATA = [
     phone: "8309-5649",
     description: "Deliciosos tequeños de queso con salsa de ajo y porciones de quesillo casero. Los venden tanto congelados como listos para eventos con la estación de Tequeños. Directo a tu puerta.",
     house: "Casa 92",
-    image: "https://imagedelivery.net/0F92Czxm-YSY0EqFzZS4jA/2194584f-67f2-4067-a5f0-d810f452c600/public",
-    imagePosition: "object-bottom",
+    image: "https://imagedelivery.net/0F92Czxm-YSY0EqFzZS4jA/cf2a116a-4ff8-483c-523f-99a397e11b00/public",
     keywords: ["tequeños", "quesillo", "comida", "postres", "queso", "venezolana", "doña ana", "deditos", "eventos", "congelados"]
   },
   {
